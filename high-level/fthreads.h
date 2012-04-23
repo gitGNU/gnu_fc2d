bdacd816421181e2f70db67c0f5cda5fdbf01bee
gonzalfj <gonzalfj@0288a2bf-96fe-4a5e-80cf-1c8b7851ba22>
2012-04-23 02:10:20 +0000

    Installed game engine works. configure and make from SVN works. Package untested.
    git-svn-id: svn+ssh://svn.code.sf.net/p/fgc/code/trunk@91 0288a2bf-96fe-4a5e-80cf-1c8b7851ba22

--- high-level/fthreads.h
+++ high-level/fthreads.h
@@ -16,6 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef G_THREADS_ENABLED
+#error FGameEngine THSYS need glib with thread support to works
+#endif
+
 #include <glib.h>
 #include "utils/red-black.h"
 
