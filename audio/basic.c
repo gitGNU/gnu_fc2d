be26c8902da41343fd93f3a3a38d002838b38fb4
gonzalfj <gonzalfj@0288a2bf-96fe-4a5e-80cf-1c8b7851ba22>
2012-05-31 22:42:41 +0000

    audio
    git-svn-id: svn+ssh://svn.code.sf.net/p/fgc/code/trunk@108 0288a2bf-96fe-4a5e-80cf-1c8b7851ba22

--- audio/basic.c
+++ audio/basic.c
@@ -20,12 +20,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <audio/basic.h>
 #include <stdio.h>
+#include <math.h>
 
 float wave_df( float* a, guint t, guint d ) {
 	int j;
 	float x=0;
 
-	for( j = t; j < (t + (WAVE_WINDOW_SIZE/2)); j++ )
+	for( j = t; j < (t + WAVE_WINDOW_SIZE); j++ )
 		x += ( ( a[j] - a[j+d] ) * ( a[j] - a[j+d] ) );
 
 	return x / WAVE_WINDOW_SIZE;
@@ -48,26 +49,183 @@ void wave_normalize( float* buf, unsigned int samples ) {
 			max = buf[i];
 	}
 	
-	fprintf(stderr, "%f\n", max);
 	for( i = 0; i < samples; i++ )
 		buf[i] *= (1. / max);
 }
 
 float wave_frequency( float* buf ) 
 {
-	guint i;
-	float t;
+	guint i, j, j2;
+	float t=0, t2=0;
+	gboolean stop = FALSE;
 	
-	for( i = 40; i < (WAVE_WINDOW_SIZE/2); i++ ) {
+	for( i = 40; i < WAVE_WINDOW_SIZE; i++ ) {
 		t = wave_df( buf, 0, i );
-		if( t < 0.01 )
-			return 44100 / i;
+
+		if( t < 0.05 ) {
+			if( t < t2 )
+				stop = TRUE;
+	
+			if( t > t2 && stop == TRUE )
+				return SR / i;
+		}
+		t2 = t;
+		
 	}
+	
+	return 0;
 }
 
 void wave_synthesize( float* buf, guint samples,
-					  guint freq, fInstrument tr ) 
+					  float freq, fInstrument tr,
+					  float a ) 
 {
+	float s=0, e=0, m=1;
+	guint i, j;
+	float f;
+	
+	f = ((M_PI*2) / SR) * freq;
+
+	if( tr == PIANO1 ) {
+		for( i = 0; i < samples; i++, e+=f ) {
+			s = FSIN(e, 1) + FSIN(e*2, 0.2) +
+			FSIN(e*3, 0.15) + FCOS(e*6, 0.12)
+			+ FCOS(e*7, 0.13) + FSIN(e*8, 0.05);
+			buf[i] = (float)((s/1.65) * (1./m) * a);
+			m += (3. /SR);
+		}
+	} else if( tr == CONSTANT1 ) {
+		for( i = 0; i < samples; i++, e+=f ) {
+			s = FSIN(e, 1) + FCOS(e*6, 0.02) +
+			FCOS(e*3, 0.5) + FSIN(e*4, 0.3) +
+			FSIN(e*9, 0.2);
+			buf[i] += (float)(s/2.02) * a;
+		}
+	} else if( tr == PERCUSSION1 ) {
+		for( i = 0; i < samples; i++, e+=f ) {
+			s = FSIN(e, 1) + FCOS(e*6, 0.02) +
+			FCOS(e*3, 0.5) + FSIN(e*4, 0.3) +
+			FSIN(e*9, 0.2);
+			buf[i] += (float)(s/2.02) * a * (1./m);
+			m += (10. /SR);
+		}
+	} else if( tr == BASS1 ) {
+		for( i = 0; i < samples; i++, e+=f ) {
+			s = FSIN(e, 1) + FCOS(e*2, 0.01) +
+			FCOS(e*3, 0.07) + FSIN(e*4, 0.03) +
+			FSIN(e*5, 0.02);
+			buf[i] += (float)(s/2.02) * a;
+		}
+	} else if( tr == BASS1_PERCUSSION ) {
+		for( i = 0; i < samples; i++, e+=f ) {
+			s = FSIN(e, 1) + FCOS(e*2, 0.01) +
+			FCOS(e*3, 0.07) + FSIN(e*4, 0.03) +
+			FSIN(e*5, 0.02);
+			buf[i] += (float)(s/2.02) * a * (1./m);
+			m += (10. /SR);
+		}
+	}
 	
 }
 
+void wave_resynthesize(float* voice, float** dest, 
+					   fInstrument tr, guint samples) 
+{
+	guint i=0, h=0;
+	guint j=0, m=0;
+	float old=0;
+	float eold=0;
+	float f=0, g, l, f2;
+	const guint w = WAVE_WINDOW_SIZE;
+
+	if( *dest == NULL )
+		*dest = g_malloc0( samples * sizeof(float) );
+
+	for( i = 0; i < samples; i+=j ) {
+		for( j = w; j+i < samples; j+= w) {
+			wave_normalize(&(voice[i+j]), w);
+			f2 = wave_frequency(&(voice[i+j]));
+			if( (f2-g)*(f2-g) < 100 )
+				f = freq_refine( g );
+			else {
+				f = freq_refine( f2 );
+				g = f2;
+			}
+			//f = f2;
+			if( f >= 1000 || f2 == 0 )
+				f = old;
+			
+			if( old != f )
+				break;
+			
+		}
+		
+		if( old != 0)  {
+			wave_synthesize( &((*dest)[i]), j, old, tr, 1 );
+			fprintf(stderr, "%f\n", old);
+		}
+		
+		old = f;
+	}
+	
+}
+
+float* wave_harmonics( float* buf ) {
+	guint i, j;
+	float* ret;
+	float freq, f, e=0;
+	
+	ret = g_malloc( sizeof(float) * 40 );
+	freq = wave_frequency( buf );
+	
+	f = ((M_PI*2) / SR) * freq;
+	
+	for( j = 0; j < 20; j++ ) {
+		ret[j] = 0;
+		ret[j+20] = 0; 
+		for( i = 0; i < WAVE_WINDOW_SIZE; i++, e+=f ) {
+			ret[j] += ( buf[i] * sin(e*(j-19)) );
+			ret[j+20] += ( buf[i] * cos(e*(j-19)) );
+		}
+		
+		ret[j] /= WAVE_WINDOW_SIZE;
+	}
+
+	return ret;
+}
+
+float freq_refine( guint f ) {
+	guint n;
+		
+	for( n = 0; freq_fromnote2(n) < f; n++ );
+	
+	return freq_fromnote2(n);
+}
+
+float freq_fromnote( fMusicalNote note, guint n ) {
+	if( note == A )
+		return FREQ_A*n;
+	else if( note == B )
+		return FREQ_B*n;
+	else if( note == C )
+		return FREQ_C*n;
+	else if( note == D )
+		return FREQ_D*n;
+	else if( note == E )
+		return FREQ_E*n;
+	else if( note == F )
+		return FREQ_F*n;
+	else if( note == G )
+		return FREQ_G*n;
+
+	return 0;
+}
+
+float freq_fromnote2( guint n ) {
+	guint i;
+	fMusicalNote nt;
+	
+	nt = n % 7;
+	
+	return freq_fromnote( nt, (n / 7)+1 );
+}
--- audio/basic.h
+++ audio/basic.h
@@ -25,27 +25,80 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SFT(x) \
 	((x) * 44100)
+	
+#define SR \
+	((float)(44100))
+	
+#define FSIN( freq, amplitude ) \
+	(sin(freq) * amplitude)
 
-#define WAVE_WINDOW_SIZE 60
+#define FCOS( freq, amplitude ) \
+	(cos(freq) * (amplitude))
+
+#define WAVE_WINDOW_SIZE (SR/60)
+
+#define FREQ_A  (264/2)
+#define FREQ_B  (297/2)
+#define FREQ_C  (330/2)
+#define FREQ_D  (352/2)
+#define FREQ_E  (396/2)
+#define FREQ_F  (440/2)
+#define FREQ_G  (495/2)
 
 /*! \brief Musical Instrument*/
 typedef enum {
-	PIANO,
-	VIOLIN,
-	FLUTE
+	PIANO1,
+	CONSTANT1,
+	PERCUSSION1,
+	BASS1,
+	BASS1_PERCUSSION
 } fInstrument;
+
+typedef enum { 
+	A, B, C, D, E, F,
+	G, DO=C, RE=D,
+	MI=E, FA=F, SOL=G,
+	LA=A, SI=B
+} fMusicalNote;
 
 float wave_df( float* a, guint t, guint d );
 
 void wave_normalize( float* buf, unsigned int samples );
 
-/*! \brief Get frequency in hertz */
+/*! \brief Get the approximate frequency in Hz */
 float wave_frequency( float* buf );
 
-/*! \brief Synthesizes the sound of a
-           musical instrument */
+/*! \brief Try to synthesizes the sound of a
+           musical instrument
+    \param buf For the result
+    \param sample Number of samplesof this sound
+    \param freq Frequence of sound(Hz)
+    \param tr The musical instrument to synthesize
+    \param a Wave amplitude
+    \
+*/
 void wave_synthesize( float* buf, guint samples, 
-					  guint freq, fInstrument tr );
+					  float freq, fInstrument tr,
+					  float a);
 
+/*! \brief Synthesizes the sound of an instrument
+ *         from the input buffer
+ *  \details Approximates the values ​​for
+ *           musical notes between(inclusive)
+ *           263Hz and 493Hz(without sharps)
+ * 
+ * \param voice The input buffer( may be your own voice )
+ * \param dest The buffer to plot the samples
+ * \param tr The musical instrument to synthesize
+ * \param samples The total number of samples of your melody
+ */
+void wave_resynthesize(float* voice, float** dest, 
+					   fInstrument tr, guint samples );
 
+float* wave_harmonics( float* buf );
+
+float freq_refine( guint f );
+
+float freq_fromnote( fMusicalNote note, guint n );
+float freq_fromnote2( guint n );
 #endif
